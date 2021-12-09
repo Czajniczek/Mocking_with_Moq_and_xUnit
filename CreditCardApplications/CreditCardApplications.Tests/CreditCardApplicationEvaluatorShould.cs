3ba2b291c76c56e01352f96528d1c1c8151dac5f
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-12-09 21:23:22 +0100

    Mocking Members of Concrete Types with Partial Mocks
    Mocking Members of Concrete Types with Partial Mocks

--- CreditCardApplications/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -304,5 +304,22 @@ namespace CreditCardApplications.Tests
             // Assert that IsValid was called three times with "aa", "bb", and "cc"
             Assert.Equal(new List<string> { "aa", "bb", "cc" }, frequentFlyerNumbersPassed);
         }
+
+        [Fact]
+        public void ReferFraudRisk()
+        {
+            var application = new CreditCardApplication();
+
+            var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+
+            var mockFraudLookup = new Mock<FraudLookup>();
+            mockFraudLookup.Setup(x => x.IsFraudRisk(It.IsAny<CreditCardApplication>()))
+                           .Returns(true);
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object, mockFraudLookup.Object);
+            var decision = sut.Evaluate(application);
+
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHumanFraudRisk, decision);
+        }
     }
 }
--- CreditCardApplications/CreditCardApplications/CreditCardApplicationDecision.cs
+++ CreditCardApplications/CreditCardApplications/CreditCardApplicationDecision.cs
@@ -5,6 +5,7 @@
         Unknown = 0, //Wynik wniosku nie jest jeszcze znany
         AutoAccepted = 1, //Wniosek został automatycznie zaakceptowany przez system
         AutoDeclined = 2, //Wniosek został automatycznie odrzucony przez system
-        ReferredToHuman = 3 //Wniosek musi zostać skierowany do człowieka
+        ReferredToHuman = 3, //Wniosek musi zostać skierowany do człowieka
+        ReferredToHumanFraudRisk = 4
     }
 }
